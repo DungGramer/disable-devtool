9717e407b14684d6ecfa19d19b36e99a6312afb8
DungGramer <dung.dev.gramer@gmail.com>
2023-10-24 01:35:33 +0700

    init project

---
+++ webpack.es.js.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'index.es.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
